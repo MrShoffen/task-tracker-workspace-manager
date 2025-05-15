01b13650937021bf4539d04f0e889ebb079a22f4
andrey_vasiliev <mrshoffen@gmail.com>
2025-05-15 20:19:25 +0300

    create release

--- src/main/java/org/mrshoffen/tasktracker/workspace/WorkspaceApplication.java
+++ src/main/java/org/mrshoffen/tasktracker/workspace/WorkspaceApplication.java
@@ -3,11 +3,15 @@ package org.mrshoffen.tasktracker.workspace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 
 public class WorkspaceApplication {
 
     public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+
         SpringApplication.run(WorkspaceApplication.class, args);
     }
 
